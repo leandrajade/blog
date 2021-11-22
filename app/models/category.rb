a0612a236db8e7ee7bacd4ac19e040a4015c9782
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-22 11:02:45 +0800

    added validations for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    validates :name, presence: true
+    validates :name, length: { minimum: 3 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe Category, type: :model do
         it '1. Is not valid without a name' do
             category.name = nil 
 
-            expect(article).to_not be_valid
-            expect(article.errors).to be_present
-            expect(article.errors.to_h.keys).to include(:name)
+            expect(category).to_not be_valid
+            expect(category.errors).to be_present
+            expect(category.errors.to_h.keys).to include(:name)
         end
 
         it '3. Is not valid with name length less than 3' do
             category.name = 'ab'
 
-            expect(article).to_not be_valid
-            expect(article.errors).to be_present
-            expect(article.errors.to_h.keys).to include(:name)
+            expect(category).to_not be_valid
+            expect(category.errors).to be_present
+            expect(category.errors.to_h.keys).to include(:name)
         end
 
     end
