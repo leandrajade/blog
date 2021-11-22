d5c40e20918bcbd340f35607c58608fe4f9c3f07
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-22 10:43:06 +0800

    added a model for category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_115630) do
+ActiveRecord::Schema.define(version: 2021_11_22_010614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_11_10_115630) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
