e7524ffd2f7437dafe3de83e7363c7d3d62a1da8
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-26 18:16:14 +0800

    fixed url for show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/articles' => 'articles#index' 
   get '/articles/new' => 'articles#new', as: 'new_article'
   post '/articles' => 'articles#create', as: 'create_article'
-  get '/articles:id' => 'articles#show', as: 'show_article'
+  get '/articles/:id' => 'articles#show', as: 'show_article'
   get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
   post '/articles/:id' => 'articles#update', as: 'update_article'
   delete '/articles/:id' => 'articles#delete', as: 'delete_article'
