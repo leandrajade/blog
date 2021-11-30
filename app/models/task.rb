b8c5e283537b177ad6738c872fe2f13e6ad0058c
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-30 15:17:58 +0800

    passed category presence and association tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
+    validates :name, presence: true
+    validates :name, length: { maximum: 20 }
+
+    validates :details, presence: true
+    validates :details, length: { maximum: 40 }
+
+    validates :scheduled_at, presence: true
+    validates :category, presence: true
 
     belongs_to :category 
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Task, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   let(:category) {Category.create(name: 'cat1')}
-  let(:task) {Task.create(category: :category, name: 'Sample name task', details: 'task details', scheduled_at: Time.new.strftime("%Y-%m-%d %H:%I:%S"))}
+  let(:task) {Task.create(category: category, name: 'Sample name task', details: 'task details', scheduled_at: Time.new.strftime("%Y-%m-%d %H:%I:%S"))}
 
   context 'Validations' do
     it "Is not valid without a name" do
@@ -28,7 +28,7 @@ RSpec.describe Task, type: :model do
 
       expect(task).to_not be_valid
       expect(task.errors).to be_present
-      expect(task.errors.to_hash.keys).to include(:details)
+      expect(task.errors.to_hash.keys).to include(:scheduled_at)
     end
 
     it 'Is not valid with characters more than 20' do
@@ -46,27 +46,19 @@ RSpec.describe Task, type: :model do
       expect(task.errors).to be_present
       expect(task.errors.to_hash.keys).to include(:details)
     end
-
-    it "Is not valid when name is not unique" do
-      task_name_duplicate = Task.create(name: 'Sample name task')
-
-      expect(task).to_not be_valid
-      expect(task.errors).to be_present
-      expect(task.errors.to_hash.keys).to include(:name)
-    end
-
+    
     it "Is not valid without a category" do
       task.category = nil
 
       expect(task).to_not be_valid
       expect(task.errors).to be_present
-      expect(task.errors.to_hash.keys).to include(:name)
+      expect(task.errors.to_hash.keys).to include(:category)
     end
   end
 
   context 'Associations' do
     it 'belongs to category' do
-      expect(task.reflect_on_association(:category).macro).to eq(:belongs_to)
+      expect(described_class.reflect_on_association(:category).macro).to eq(:belongs_to)
     end
   end
 end
